0cad652bd44df56b0b444ab1886701acfe85d6f2
Yu Lu <yulu@utexas.edu>
2017-10-22 14:40:48 -0500

    fix random order of table

--- app/songs.py
+++ app/songs.py
@@ -9,9 +9,9 @@
 # 
 # Created: Sat Oct 21 13:24:45 2017 (-0500)
 # Version: 
-# Last-Updated: Sun Oct 22 14:16:30 2017 (-0500)
+# Last-Updated: Sun Oct 22 14:39:49 2017 (-0500)
 #           By: yulu
-#     Update #: 25
+#     Update #: 26
 # 
 
 
@@ -26,7 +26,7 @@ def songs():
     json_url = os.path.join(SITE_ROOT, "static/json", "Songs.json")
     with codecs.open(json_url, 'r', 'utf-8-sig') as f:
         data = json.load(f)
-    tableTitles = data[0].keys()
+    tableTitles = ['NAME', 'DURATION', 'BAND', 'ALBUM', 'GENRE', 'YEAR_OF_RELEASE', 'LYRICS_LINK', 'VIDEO_LINK']
     return render_template("songs.html",
                            tableTitles = tableTitles,
                            data = data)
