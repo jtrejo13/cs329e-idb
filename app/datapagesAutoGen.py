3ae9828e1e63c81a074c1ccf2411ad0253c1e6c6
Yu Lu <yulu@utexas.edu>
2017-10-25 22:54:08 -0500

    remove unnecessary packages

--- app/datapagesAutoGen.py
+++ app/datapagesAutoGen.py
@@ -1,6 +1,5 @@
 from app import app
-import codecs,json, os
-from flask import Flask, url_for, render_template
+from flask import render_template
 
 @app.route('/database/<name>')
 def database(name=None):
