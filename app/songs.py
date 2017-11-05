e6d2f1f6a2e1f41b746a08dcf4708ea533d4ddc9
tarrantrl <31635666+tarrantrl@users.noreply.github.com>
2017-11-05 17:09:22 -0600

    Updated songs.py to use database

--- app/songs.py
+++ app/songs.py
@@ -1,34 +1,18 @@
-# songs.py --- 
-# 
-# Filename: songs.py
-# Description: 
-# 
-# Author:    Yu Lu
-# Email:     yulu@utexas.edu
-# Github:    https://github.com/SuperYuLu 
-# 
-# Created: Sat Oct 21 13:24:45 2017 (-0500)
-# Version: 
-# Last-Updated: Wed Oct 25 22:13:43 2017 (-0500)
-#           By: yulu
-#     Update #: 29
-# 
-
-
 from app import app
-import codecs,json, os
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, request, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Songs, engine
+from build_db import create_songs, session
 
 @app.route('/songs')
 def songs():
-    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-    json_url = os.path.join(SITE_ROOT, "static/json", "Songs.json")
-    with codecs.open(json_url, 'r', 'utf-8-sig') as f:
-        data = json.load(f)
+    songs = session.query(Songs)
     tableTitles = ['NAME', 'DURATION', 'BAND', 'ALBUM', 'GENRE', 'YEAR_OF_RELEASE', 'LYRICS_LINK', 'VIDEO_LINK']
     return render_template("songs.html",
                            tableTitles = tableTitles,
-                           data = data)
+                           songs = songs)
+
 
 
 
