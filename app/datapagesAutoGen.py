f40cf6fada143d0606be68a2081c03c966a184cb
tarrantrl-lt\Rachel <tarrantrl@utexas.edu>
2017-11-07 12:42:14 -0600

    added links to songs.html, created artist and song template

--- app/datapagesAutoGen.py
+++ app/datapagesAutoGen.py
@@ -15,8 +15,25 @@
 #
 
 from app import app
-from flask import render_template
+from flask import Flask, render_template, request, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models import Base, Songs, Artists, Albums, engine
+from app.build_db import create_songs, create_artists, create_albums, session
 
-@app.route('/database/<name>')
-def database(name=None):
-    return render_template(name+'.html')
+songs = session.query(Songs)
+artists = session.query(Artists)
+albums = session.query(Albums)
+
+
+@app.route('/database/song/<title>')
+def database(title=None):
+    return render_template('song-indiv.html', songs = songs, title = title)  
+    
+@app.route('/database/artist/<artist>')
+def database2(artist=None):
+    return render_template('artist-indiv.html', artists = artists, artist = artist)
+
+@app.route('/database/album/<album>')
+def database3(album=None):
+    return render_template('album-indiv.html', albums = albums, album = album)
