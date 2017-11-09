6be35340b19e2720cb69cf6100e3b1a3a0604289
Yu Lu <yulu@utexas.edu>
2017-11-09 11:19:27 -0600

    fixed bunch of stuff

--- app/datapagesAutoGen.py
+++ app/datapagesAutoGen.py
@@ -9,9 +9,9 @@
 #
 # Created: Wed Oct 25 23:11:53 2017 (-0500)
 # Version:
-# Last-Updated: Wed Oct 25 23:15:15 2017 (-0500)
+# Last-Updated: Thu Nov  9 11:00:20 2017 (-0600)
 #           By: yulu
-#     Update #: 9
+#     Update #: 18
 #
 
 from app import app
@@ -22,20 +22,25 @@ from app.models import Base, Songs, Artists, Albums, engine
 from app.build_db import create_songs, create_artists, create_albums, session
 
 
-@app.route('/database/song/<title>')
+@app.route('/song/<title>')
 def database(title=None):
     songs = session.query(Songs)
-    return render_template('song-indiv.html', songs = songs, title = title)  
+    albums = session.query(Albums)
+    return render_template('song-indiv.html', songs = songs, title = title, albums = albums)  
     
-@app.route('/database/artist/<artist>')
+    
+@app.route('/artist/<artist>')
 def database2(artist=None):
     artists = session.query(Artists)
-    return render_template('artist-indiv.html', artists = artists, artist = artist)
+    albums = session.query(Albums)
+    songs = session.query(Songs)
+    return render_template('artist-indiv.html', artists = artists, artist = artist, songs = songs, albums = albums)
 
-@app.route('/database/album/<album>')
+@app.route('/album/<album>')
 def database3(album=None):
     albums = session.query(Albums)
+    songs = session.query(Songs)
     #for dic in albums:
     #    if ''.join(dic['name'].split()).lower() == album:
     #        album_dic = dic
-    return render_template('album-indiv.html', albums = albums, album = album)
+    return render_template('album-indiv.html', albums = albums, album = album, songs = songs)
