e20f52bed919683c8238658a70d36e4a55f84036
Yu Lu <yulu@utexas.edu>
2017-11-17 00:46:02 -0600

    add table search test in songs.html

--- app/static/JavaScript/tableConfig.js
+++ app/static/JavaScript/tableConfig.js
@@ -2,7 +2,10 @@ $(function(){
     $("table").tablesorter({
 	theme: 'blue',
 	sortList: [[0,0], [1,0]],
-	widgets: ['zebra'],
+	widgets: ['zebra', 'filter'],
+	widgetOptions:{
+	    filter_external: '.search'
+	}
     });
     $("table").tablesorterPager({
 	container: $("#pager"),
