2234dd8ade47ff2844f55735bf1fda4d6d893809
Yu Lu <yulu@utexas.edu>
2017-11-18 16:47:53 -0600

    made live search work for songs

--- app/static/JavaScript/tableConfig.js
+++ app/static/JavaScript/tableConfig.js
@@ -4,11 +4,25 @@ $(function(){
 	sortList: [[0,0], [1,0]],
 	widgets: ['zebra', 'filter'],
 	widgetOptions:{
-	    filter_external: '.search'
+	    filter_external: '.search',
+	    filter_columnFilters: false,
+	    filter_reset: '.reset',
+	    filter_ignoreCase: true,
+	    filter_liveSearch: true,
+	    filter_searchDelay: 100,
 	}
     });
+
+    
     $("table").tablesorterPager({
 	container: $("#pager"),
+    });
+
+    $('select').change(function(){
+	// modify the search input data-column value (swap "0" or "all")
+	$('.selectable').attr( 'data-column', $(this).val() );
+	// update external search inputs
+	$.tablesorter.filter.bindSearch( $table, $('.search'), false );
     });
 });
 
